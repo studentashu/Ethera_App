6f37c0fdc24e70dee7e96bc5e45337baaa410d42
studentashu <ponnuashwini6@gmail.com>
2026-05-02 12:32:01 +0530

    added edit and updated server

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api" 
-// || "https://etheraapp-production.up.railway.app/api"
+  baseURL: //"http://localhost:5000/api" 
+ "https://etheraapp-production.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
