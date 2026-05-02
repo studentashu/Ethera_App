19ec51677a03dd2ec07e4412c50884df55fe1c11
studentashu <146846176+studentashu@users.noreply.github.com>
2026-05-02 11:22:15 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://etheraapp-production.up.railway.app/"
+  baseURL: "https://etheraapp-production.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
